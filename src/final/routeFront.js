a79ee08e08a5e76519227a494eaac6dc5f6cec06
Gonzalo Nandez <godan2310@yahoo.es>
2018-03-23 01:55:29 +0100

    Solved #23

--- src/final/routeFront.js
+++ src/final/routeFront.js
@@ -7,11 +7,11 @@ const routes =
       path: '/basic',
       name: 'basic',
       component: basic
-    },
-    {
+    }, {
       path: '/Example',
       name: 'Example',
       component: Example
     }
   ]
+
 export default routes
